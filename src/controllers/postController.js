85292014dddf320c0dae590b125b650169bc69b7
Luqman Adeniyi <luqmanadeniyi97@gmail.com>
2023-07-24 14:44:40 +0100

    changed my query statement

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -5,15 +5,15 @@ const { getUserIdFromRefreshToken } = require("../utils/helpers");
 // Getting all posts from the db
 const getPosts = async (req, res) => {
   const sqlQuery = `
-  SELECT * FROM authdb.users
-  LEFT JOIN authdb.posts ON users.id = posts.author;
+  SELECT posts.*, users.id AS userId, users.fullName FROM posts
+  LEFT JOIN users ON users.id = posts.author;
 `;
   db.query(sqlQuery, (err, results) => {
     if (err) return res.status(401).json(err.message);
     console.log(results);
     const newData = results.map((result) => {
-      const { password, email, fullName, author, ...newobj } = result;
-      return { ...newobj, author: { fullName, email, author_id: author } };
+      const { fullName, author, ...newobj } = result;
+      return { ...newobj, author: { fullName, author_id: author } };
     });
     res.status(200).json({ message: "success", result: newData });
   });
@@ -22,14 +22,14 @@ const getPosts = async (req, res) => {
 // Getting specified post from the db
 const getPost = async (req, res) => {
   const { id } = req.params;
-  const sqlQuery = `SELECT * FROM authdb.users
-  LEFT JOIN authdb.posts ON users.id = posts.author WHERE posts.id = ?`;
+  const sqlQuery = `SELECT posts.*, users.id AS userId, users.fullName FROM posts
+  LEFT JOIN users ON users.id = posts.author WHERE posts.id = ?`;
 
   db.query(sqlQuery, [id], (err, results) => {
     if (err) return res.status(401).json(err.message);
     const newData = results.map((result) => {
-      const { password, email, fullName, author, ...newobj } = result;
-      return { ...newobj, author: { fullName, email, author_id: author } };
+      const { fullName, author, ...newobj } = result;
+      return { ...newobj, author: { fullName, author_id: author } };
     });
     res.status(200).json({ message: "success", data: newData });
   });
@@ -62,8 +62,8 @@ const createPost = async (req, res) => {
 
   db.query(sqlQuery, [data], (err, result) => {
     if (err) return res.status(401).json(err.message);
-    let q = "SELECT * FROM posts";
-    db.query(q, [userId], (err, result) => {
+    let q = "SELECT * FROM posts WHERE id = ?";
+    db.query(q, [result.insertId], (err, result) => {
       if (err) return res.status(401).json(err.message);
       return res
         .status(200)
@@ -91,15 +91,16 @@ const updatePost = async (req, res) => {
   console.log(title, description, image);
   const sqlQuery = `UPDATE posts SET title = ?, description = ?, image = ? WHERE id = ?`;
   const q = `
-  SELECT * FROM authdb.posts LEFT JOIN authdb.users ON posts.author = users.id WHERE posts.id = ?;
+  SELECT posts.*, users.id AS userId, users.fullName FROM posts
+  LEFT JOIN users ON users.id = posts.author WHERE posts.id = ?;
 `;
   db.query(sqlQuery, [title, description, image, id], async (err, result) => {
     if (err) return res.status(401).json(err.message);
 
     db.query(q, [id], (err, results) => {
       const newData = results.map((result) => {
-        const { password, email, fullName, author, ...newobj } = result;
-        return { ...newobj, author: { fullName, email, author_id: author } };
+        const { fullName, author, ...newobj } = result;
+        return { ...newobj, author: { fullName, author_id: author } };
       });
       if (err) return res.status(401).json(err.message);
       return res
