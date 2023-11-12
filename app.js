3ce214f6f95030cbc513ce9b7ced2c42359b7857
Luqman Adeniyi <luqmanadeniyi97@gmail.com>
2023-11-12 17:58:38 +0100

    fixed cors error

--- app.js
+++ app.js
@@ -10,7 +10,15 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+  origin: 'https://node-blog-api-n5uy.onrender.com',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions));
+
 
 app.use(logger);
 
